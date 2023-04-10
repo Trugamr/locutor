87497ae529a7c7b8808592901722a6d505923932
Trugamr <trugamr@live.in>
2023-04-10 20:33:46 +0530

    feature: play message according to voice event

--- src/events/voice-state-update.ts
+++ src/events/voice-state-update.ts
@@ -9,6 +9,38 @@ import { Voice } from '../services/voice'
 import Event from '../structs/event'
 import TYPES from '../types'
 
+enum VoiceStateEventType {
+  Joined,
+  Left,
+  Changed,
+  Unchanged,
+}
+
+function getVoiceStateEventType(prev: VoiceState, next: VoiceState) {
+  if (!prev.channel && next.channel) {
+    invariant(next.member, 'member must be defined')
+    return {
+      type: VoiceStateEventType.Joined,
+      member: next.member,
+    } as const
+  } else if (prev.channel && !next.channel) {
+    invariant(prev.member, 'member must be defined')
+    return {
+      type: VoiceStateEventType.Left,
+      member: prev.member,
+    } as const
+  } else if (prev.channel && next.channel) {
+    invariant(next.member, 'member must be defined')
+    return {
+      type: VoiceStateEventType.Changed,
+      member: next.member,
+    } as const
+  }
+  return {
+    type: VoiceStateEventType.Unchanged,
+  } as const
+}
+
 @injectable()
 export default class VoiceStateUpdate implements Event<Events.VoiceStateUpdate> {
   readonly type = Events.VoiceStateUpdate
@@ -23,35 +55,22 @@ export default class VoiceStateUpdate implements Event<Events.VoiceStateUpdate> 
   ) {}
 
   readonly listener = async (prev: VoiceState, next: VoiceState) => {
-    // TODO: Only play if user is joining or leaving same channel as bot
-    if (!prev.channel && next.channel) {
-      // User joined a voice channel
-      this.logger.debug(
-        `User ${next.member?.user.username} joined voice channel ${next.channelId}`,
-      )
-    } else if (prev.channel && !next.channel) {
-      // User left a voice channel
-      this.logger.debug(
-        `User ${prev.member?.user.username} left voice channel ${prev.channelId}`,
-      )
-    } else if (prev.channel && next.channel) {
-      // User changed voice channel
-      this.logger.debug(
-        `User ${next.member?.user.username} changed voice channel from ${prev.channelId} to ${next.channelId}`,
-      )
-    }
-
+    // Bot user must be defined on client
     const me = this.client.user
     invariant(me, 'client user must be defined')
 
-    // Find the voice channel the bot is currently in
+    // Don't do anything if bot is not the in one of the voice channels
+    if (![prev.channel, next.channel].some(c => c?.members.find(m => m.id === me.id))) {
+      return
+    }
+
+    // Don't do anything if bot is not in any voice channel
     const channel = this.client.channels.cache.find(channel => {
       if (channel.type === ChannelType.GuildVoice) {
         return channel.members.find(member => member.id === me.id)
       }
       return false
     })
-
     if (!channel) {
       return
     }
@@ -67,11 +86,27 @@ export default class VoiceStateUpdate implements Event<Events.VoiceStateUpdate> 
     url.searchParams.set('ie', 'UTF-8')
     url.searchParams.set('tl', 'hi-IN')
     url.searchParams.set('client', 'tw-ob')
-    url.searchParams.set('q', 'bop')
+
+    // TODO: Make messages user configurable
+
+    // Build message to play
+    const event = getVoiceStateEventType(prev, next)
+    if (event.type === VoiceStateEventType.Joined) {
+      url.searchParams.set('q', `${event.member.displayName} चैनल में आ चुके है`)
+    } else if (event.type === VoiceStateEventType.Left) {
+      url.searchParams.set('q', `${event.member.displayName} चैनल छोड़ कर चले गए है`)
+    } else if (event.type === VoiceStateEventType.Changed) {
+      url.searchParams.set('q', `${event.member.displayName} ने चैनल बदल लिया है`)
+    } else {
+      return
+    }
 
     const resource = createAudioResource(url.href, {
       inputType: StreamType.Arbitrary,
+      inlineVolume: true,
     })
+    invariant(resource.volume, 'resource volume must be defined')
+    resource.volume.setVolume(0.5)
 
     player.play(resource)
   }
